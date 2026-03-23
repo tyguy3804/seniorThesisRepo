d3cb7c8ffc9fd6abc3bcdb6a81172370b6894026
tyguy3804 <twojdyla@carthage.edu>
2026-03-23 13:50:34 -0500

    Begin working on some models, starting with a Decision Tree Classifier

--- code/dataset_creation.py
+++ code/dataset_creation.py
@@ -1,6 +1,4 @@
-#import metpy_calculations
-import metpy.calc as mpcalc
-from metpy.units import units
+import metpy_calculations
 import numpy as np
 import calendar
 import zipfile
@@ -15,202 +13,6 @@ import os
 import traceback
 from collections import defaultdict
 
-def get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= 'all'):
-
-    if variables == 'all':
-        variables = ['pressure', 'dewpt', 'temp','t2m', 'sp', 'u_wind', 'v_wind', 'geopotential_agl']
-    
-    results = {}
-    
-    if 'pressure' in variables:
-        p = ds_press.variables['pressure_level'][:]
-        pressure = p.filled(np.nan) * units.hPa
-        results['pressure'] = pressure
-
-    if 'sp' in variables:
-        sp = (ds_sur.variables['sp'][time, lat_idx, long_idx] / 100) * units.hPa
-        results['sp'] = sp
-    
-    if 'dewpt' in variables:
-        q = ds_press.variables['q'][time, :, lat_idx, long_idx]
-        spec_humidity = q.filled(np.nan) * units('kg/kg')
-        spec_humidity = spec_humidity * 1000
-        spec_humidity = spec_humidity * units('g/kg')
-        dewpt = mpcalc.dewpoint_from_specific_humidity(pressure, spec_humidity)
-        results['dewpt'] = dewpt
-
-    if 'temp' in variables:
-        t = ds_press.variables['t'][time, :, lat_idx, long_idx]
-        temp = t.filled(np.nan) * units.kelvin
-        temp = temp.to('degC')
-        results['temp'] = temp
-
-    if 't2m' in variables:
-        t2m = ds_sur.variables['t2m'][time, lat_idx, long_idx] * units.kelvin
-        results['t2m'] = t2m
-
-    if 'u_wind' in variables and 'v_wind' in variables:
-        u = ds_press.variables['u'][time, :, lat_idx, long_idx]
-        v = ds_press.variables['v'][time, :, lat_idx, long_idx]
-        u_wind = u.filled(np.nan) * units('m/s')
-        v_wind = v.filled(np.nan) * units('m/s')
-        results['u_wind'] = u_wind
-        results['v_wind'] = v_wind
-    
-    if 'geopotential_agl' in variables:
-        z = ds_press.variables['z'][time, :, lat_idx, long_idx]
-        geopotential = z.filled(np.nan) / 9.80665
-        geopotential_height = geopotential * units('m')
-        geopotential_agl = geopotential_height - geopotential_height[0]
-        results['geopotential_agl'] = geopotential_agl
-
-    return results
-
-def era5_calculate_pwat(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt'])
-
-    pressure = data['pressure']
-    dewpt = data['dewpt']
-
-    pwat = mpcalc.precipitable_water(pressure, dewpt)
-    return pwat
-
-def era5_calculate_lcl(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp'])   
-
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    lcl_press, lcl_temp = mpcalc.lcl(pressure[0], temp[0], dewpt[0])
-    return lcl_press, lcl_temp
-
-def era5_calculate_lfc(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp']) 
-
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    lfc_press, lfc_temp = mpcalc.lfc(pressure, temp, dewpt)
-    return lfc_press, lfc_temp
-
-def era5_calculate_lapse_rates(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp']) 
-
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    lapse_rates = mpcalc.dry_lapse(pressure, temp[0]).to('degC')
-
-    return lapse_rates
-
-def era5_calculate_showalter_idx(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp']) 
-
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    showalter_idx = mpcalc.showalter_index(pressure, temp, dewpt)
-
-    return showalter_idx
-
-def era5_calculate_ml_CAPE_CIN(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp']) 
-
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    ml_CAPE, ml_CIN = mpcalc.mixed_layer_cape_cin(pressure, temp, dewpt)
-
-    return ml_CAPE, ml_CIN
-
-def era5_calculate_sb_CAPE_CIN(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp']) 
-    
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    sb_CAPE, sb_CIN = mpcalc.surface_based_cape_cin(pressure, temp, dewpt)
-
-    return sb_CAPE, sb_CIN
-
-def era5_calculate_mu_CAPE_CIN(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'dewpt', 'temp']) 
-
-    pressure = data['pressure']
-    temp = data['temp']
-    dewpt = data['dewpt']
-
-    mu_CAPE, mu_CIN = mpcalc.most_unstable_cape_cin(pressure, temp, dewpt)
-
-    return mu_CAPE, mu_CIN
-
-def era5_calculate_bulk_shear(ds_press, ds_sur, time, lat_idx, long_idx, depth_m):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'u_wind', 'v_wind', 'geopotential_agl'])
-
-    pressure = data['pressure']
-    u_wind = data['u_wind']
-    v_wind = data['v_wind']
-    geo_agl = data['geopotential_agl']
-
-    u_shear, v_shear = mpcalc.bulk_shear(
-        pressure,
-        u_wind,
-        v_wind,
-        height = geo_agl,
-        bottom = 0 * units.m,
-        depth = depth_m * units.m
-    )
-
-    bulk_shear = np.sqrt(u_shear**2 + v_shear**2)
-
-    return bulk_shear
-
-def era5_calculate_srh(ds_press, ds_sur, time, lat_idx, long_idx):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['pressure', 'u_wind', 'v_wind', 'geopotential_agl'])
-
-    pressure = data['pressure']
-    u_wind = data['u_wind']
-    v_wind = data['v_wind']
-    geo_agl = data['geopotential_agl']
-
-    (u_storm, v_storm), *_ = mpcalc.bunkers_storm_motion(pressure, u_wind, v_wind, geo_agl)
-    *_, total_helicity = mpcalc.storm_relative_helicity(
-        geo_agl,
-        u_wind,
-        v_wind,
-        depth= 1000 * units.m,
-        storm_u= u_storm,
-        storm_v= v_storm
-    )
-
-    return total_helicity
-
-def era5_calculate_sig_tor(ds_press, ds_sur, time, lat_idx, long_idx, sb_CAPE, lcl_press, lcl_temp, total_helicity, bulk_shear_0to6km):
-    data = get_era5_variables(ds_press, ds_sur, time, lat_idx, long_idx, variables= ['sp', 't2m'])
-
-    sp = data['sp']
-    t2m = data['t2m']
-    
-    t_avg = (t2m + (lcl_temp.to('kelvin'))) / 2
-    lcl_height =  (287 * units('(m**2/s**2) / kelvin')) * t_avg
-    lcl_height = lcl_height / (9.81 * units('m/s**2'))
-    log_temp = sp / lcl_press
-    log_temp = math.log(log_temp)
-    lcl_height = lcl_height * log_temp
-
-    sig_tor_parameter = mpcalc.significant_tornado(sb_CAPE, lcl_height, total_helicity, bulk_shear_0to6km)
-
-    return sig_tor_parameter
-
-#-----------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------
-#-----------------------------------------------------------------------------------------
 
 def find_storm_reports(month, storm_reports):
     all_reports = []
@@ -414,18 +216,18 @@ def process_month_features(monthIdx, year):
                 for lon_idx in range(0,35):
                     
 
-                    sb_CAPE, sb_CIN = era5_calculate_sb_CAPE_CIN(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    lcl_press, lcl_temp = era5_calculate_lcl(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    total_helicity = era5_calculate_srh(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    bulk_shear_0to6km = era5_calculate_bulk_shear(ds_press, ds_sur, time, lat_idx, lon_idx, 6000)
-                    bulk_shear_0to3km = era5_calculate_bulk_shear(ds_press, ds_sur, time, lat_idx, lon_idx, 3000)
-                    mu_CAPE, mu_CIN = era5_calculate_mu_CAPE_CIN(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    pwat = era5_calculate_pwat(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    lfc_press, lfc_temp = era5_calculate_lfc(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    lapse_rates = era5_calculate_lapse_rates(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    showalter_idx = era5_calculate_showalter_idx(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    ml_CAPE, ml_CIN = era5_calculate_ml_CAPE_CIN(ds_press, ds_sur, time, lat_idx, lon_idx)
-                    sig_tor = era5_calculate_sig_tor(ds_press, ds_sur, time, lat_idx, lon_idx, sb_CAPE, lcl_press, lcl_temp, total_helicity, bulk_shear_0to6km)
+                    sb_CAPE, sb_CIN = metpy_calculations.era5_calculate_sb_CAPE_CIN(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    lcl_press, lcl_temp = metpy_calculations.era5_calculate_lcl(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    total_helicity = metpy_calculations.era5_calculate_srh(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    bulk_shear_0to6km = metpy_calculations.era5_calculate_bulk_shear(ds_press, ds_sur, time, lat_idx, lon_idx, 6000)
+                    bulk_shear_0to3km = metpy_calculations.era5_calculate_bulk_shear(ds_press, ds_sur, time, lat_idx, lon_idx, 3000)
+                    mu_CAPE, mu_CIN = metpy_calculations.era5_calculate_mu_CAPE_CIN(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    pwat = metpy_calculations.era5_calculate_pwat(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    lfc_press, lfc_temp = metpy_calculations.era5_calculate_lfc(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    lapse_rates = metpy_calculations.era5_calculate_lapse_rates(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    showalter_idx = metpy_calculations.era5_calculate_showalter_idx(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    ml_CAPE, ml_CIN = metpy_calculations.era5_calculate_ml_CAPE_CIN(ds_press, ds_sur, time, lat_idx, lon_idx)
+                    sig_tor = metpy_calculations.era5_calculate_sig_tor(ds_press, ds_sur, time, lat_idx, lon_idx, sb_CAPE, lcl_press, lcl_temp, total_helicity, bulk_shear_0to6km)
                     #supercell_comp = metpy_calculations.era5_calculate_supercell_comp(ds_press, ds_sur, time, lat_idx, lon_idx, mu_CAPE)
 
                 
@@ -517,7 +319,8 @@ def generateprocesses():
 
 
 #process_year_labels(2011)
-generateprocesses()
+#generateprocesses()
+process_month_features(3,2010)
 
 #storm_reports_path = f"C:/Users/lwojd/Data/stormReports/StormEvents_d2007_oklahoma.csv"
 #storm_reports = pd.read_csv(storm_reports_path)
--- code/era5_model_testing.py
+++ code/era5_model_testing.py
@@ -1,6 +1,145 @@
+import pandas as pd
+import os
+import re
+import matplotlib.pyplot as plt
+from sklearn.model_selection import cross_val_score
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import export_graphviz
+from graphviz import Source
+from sklearn.preprocessing import StandardScaler
+
+def clean_features(features):
+    #we need to remove lapse rates column because we cannot have a list in the column in order to scale all the features
+    #therefore we need to add three more columns, (low_level_lr, mid_level_lr, and high_level_lr), so that the most important data from the lapse rate list is still used for the model
+    #we need to change sig-tor value from also being a list
+
+    features = features.drop('year', axis=1)
+    features.insert(12, 'upper_lvl_lr', 0)
+    features.insert(12, 'mid_lvl_lr', 0)
+    features.insert(12, 'sur_lvl_lr', 0)
+
+    features['sur_lvl_lr'] = features['lapse_rates'].apply(lambda x: x[10])
+    features['mid_lvl_lr'] = features['lapse_rates'].apply(lambda x: x[6])
+    features['upper_lvl_lr'] = features['lapse_rates'].apply(lambda x: x[2])
+
+    features['showalter_idx'] = features['showalter_idx'].apply(lambda x: x[0])
+    features['sig_tor'] = features['sig_tor'].apply(lambda x: x[0])
+    features = features.drop('lapse_rates', axis=1)
+
+
+    return features
+
+def clean_labels(labels):
+
+    #We currently have some rows in the label dataframe with mutiple storm event types that the model treats as unique classifications.
+    #So set a hierarchy with Tornado, Funnel Cloud, Hail, and Thunderstorm Wind being the only unique classifications
+    #Also make sure that tornado_mag, lat_idx, lon_idx are also only 
+
+    priority = ['Tornado', 'Funnel Cloud', 'Hail', 'Thunderstorm Wind']
+
+    def get_primary_event(event_str):
+        for p in priority:
+            if p.lower() in event_str.lower():
+                return p
+        return 'N/A'
+    
+    def get_primary_mag(mag_str):
+        if pd.isna(mag_str):
+            return None
+        
+        match = re.search(r'EF?\d', mag_str, re.IGNORECASE)
+        return match.group(0) if match else None
+    
+    labels['storm_event_type'] = labels['storm_event_type'].apply(get_primary_event)
+    labels['storm_lat'] = labels['storm_lat'].str.split(',').str[0].str.strip() 
+    labels['storm_lon'] = labels['storm_lon'].str.split(',').str[0].str.strip() 
+    labels['tornado_mag'] = labels['tornado_mag'].apply(get_primary_mag)
+
+    return labels
+
+def combine_monthly_df():
+
+    print('Combining data from 2007-2011 into feature and label lists.')
+    df_features = []
+    df_labels = []
+
+    for year in range(2007, 2012):
+        for month in range(1, 13):
+
+            feature_path = f"C:/Users/lwojd/Data/metpyCalc/featureData/{year}/{month:02d}/features{month:02d}_{year}.parquet"
+            label_path = f"C:/Users/lwojd/Data/metpyCalc/labelData/{year}/{month:02d}/labels{month:02d}_{year}.parquet"
+
+            if os.path.exists(feature_path) and os.path.exists(label_path):
+                feature_file = pd.read_parquet(feature_path)
+                label_file = pd.read_parquet(label_path)
+
+                feature_new = clean_features(feature_file)
+                label_new = clean_labels(label_file)
+
+                df_features.append(feature_new)
+                df_labels.append(label_new)
+
+    return df_features, df_labels
 
 
 
+def concat_df_full(df_features, df_labels):
+
+    print('Turning feature and label data lists into Dataframes.')
+    df_features_combined = pd.concat(df_features, ignore_index=True)
+    df_labels_combined = pd.concat(df_labels, ignore_index=True)
+
+    return df_features_combined, df_labels_combined
+
+def feature_scaling(train_df, test_df):
+    #scale the features by using standardization
+    print("Feature Scaling in progress.")
+    print(train_df)
+
+    scaler = StandardScaler().fit(train_df)
+    scaled_feature_train = scaler.transform(train_df)
+
+    return scaled_feature_train
+
+def main():
+
+    df_features_month, df_labels_month = combine_monthly_df()
+    df_features_year, df_labels_year = concat_df_full(df_features_month, df_labels_month)
+
+    split_idx = int(len(df_features_year) * 0.80)
+    
+    feature_train_df = df_features_year.iloc[:split_idx]
+    feature_test_df = df_features_year.iloc[split_idx:]
+
+    label_train_df = df_labels_year.iloc[:split_idx]
+    label_test_df = df_labels_year.iloc[split_idx:]
 
     
+
+    #tree_clf = DecisionTreeClassifier()
+    print('Creating tree_clf.')
+    tree_clf = DecisionTreeClassifier(max_depth= 10)
+    #tree_clf = DecisionTreeClassifier(max_depth= 15)
+    #tree_clf = DecisionTreeClassifier(max_depth= 20)
+    print('Fitting treec_clf to data.')
+    tree_clf = tree_clf.fit(feature_train_df, label_train_df['storm_event_type'])
+
+    predict_row = feature_test_df.iloc[[100]]
+    predict_row_label = label_test_df.iloc[[100]]
+
+    print('Prediction: ', tree_clf.predict(predict_row))
+    print('Actual Storm Report: ', predict_row_label)
+    print('Training Score: ',tree_clf.score(feature_train_df, label_train_df['storm_event_type']))
+    print('Test Score: ',tree_clf.score(feature_test_df, label_test_df['storm_event_type']))
+    print('Tree depth: ', tree_clf.get_depth())
+    print('Num leaves: ', tree_clf.get_n_leaves())
+
+
+    export_graphviz(tree_clf, 
+                out_file='C:/Users/lwojd/Downloads/tree1.dot',
+                max_depth=12,
+                feature_names=feature_train_df.columns.tolist(),
+                class_names=tree_clf.classes_.tolist(),
+                filled=True)
     
+main()
